7ff348ead446ed639718fffff3885a6248b39ae0
wnchstrr <haligali1592@gmail.com>
2026-05-20 22:23:41 +0500

    feat: add user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import User, UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Регистрирует нового пользователя"""
+    if request.method != 'POST':
+        # Вывод пустой формы регистрации.
+        form = UserCreationForm()
+    else:
+        # Обработка заполненной формы.
+        form = UserCreationForm(data=request.POST)
+
+    if form.is_valid():
+        new_user = form.save()
+        # Выполнение входа и перенаправление на новую страницу.
+        login(request, new_user)
+        return redirect('learning_logs: index')
+
+    # Вывод пустой или недействительной формы.
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,4 @@ STATIC_URL = "static/"
 
 # Мои настройки.
 LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
