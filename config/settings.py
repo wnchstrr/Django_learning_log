967949ef024cd4623511b8189585d00b2feb78b5
wnchstrr <haligali1592@gmail.com>
2026-05-29 21:44:07 +0500

    feat(templates): add custom 404 and 500 error pages

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     "learning_logs",
     "accounts",
     # Сторонние приложения
-    "django_bootstrap5",
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +67,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -130,32 +130,33 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Мои настройки.
-LOGIN_REDIRECT_URL = "learning_logs:index"
-LOGOUT_REDIRECT_URL = "learning_logs:index"
-LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
 
 
-# Настройки Platform.sh.
+ # Настройки Platform.sh.
 from platformshconfig import Config
 
 config = Config()
 if config.is_valid_platform():
-    ALLOWED_HOSTS.append(".platformsh.site")
+    ALLOWED_HOSTS.append('.platformsh.site')
+    DEBUG = False
 
     if config.addDir:
-        STATIC_ROOT = Path(config.addDir) / "static"
+        STATIC_ROOT = Path(config.addDir) / 'static'
     if config.projectEntropy:
         SECRET_KEY = config.projectEntropy
 
     if not config.in_build:
-        db_settings = config.credentials("database")
+        db_settings = config.credentials('database')
         DATABASES = {
-            "default": {
-                "ENGINE": "django.db.backends.postgresql",
-                "NAME": db_settings["path"],
-                "USER": db_settings["username"],
-                "PASSWORD": db_settings["password"],
-                "HOST": db_settings["host"],
-                "PORT": db_settings["port"],
-            },
-        }
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': db_settings['path'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
+                'HOST': db_settings['host'],
+                'PORT': db_settings['port'],
+            }, 
+        }    
